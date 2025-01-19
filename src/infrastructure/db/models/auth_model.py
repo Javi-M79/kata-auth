a561b5a68ea867eee97dfd03b68f928ac60c07ba
Silx79 <javier.mira46@gmail.com>
2025-01-19 13:03:57 +0100

    update auth_model .py

---
+++ src/infrastructure/db/models/auth_model.py
@@ -0,0 +1,18 @@
+import uuid
+
+from peewee import Model, UUIDField, TextField, TimestampField
+
+from infrastructure.db.database import db
+
+#Model representa una tabla dentro de la base de datos. Cada instancia de la clase corresponde a una fila.
+class AuthEntity(Model):
+    id = UUIDField(primary_key=True, default=uuid.uuid4)  # Identificador unico
+    refresh_token = TextField(null=False)  # Almacena el Token de refresco
+    user_id = UUIDField(null=False)  # id de usuario asociado al que pertenece el token.
+    emited_date = TimestampField(null =False)   #Fecha de emision del token.
+    expired_date = TimestampField(null=False)  # Fecha de expiracion del token
+    update_date = TimestampField(null=False)  # Fecha de actualizacion del token
+
+    class Meta:
+        database = db
+        table_name = 'auth'
