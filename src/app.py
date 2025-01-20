ac7bd1b69ba2ada9a6060827d98a55dcd085d429
Silx79 <javier.mira46@gmail.com>
2025-01-20 21:36:04 +0100

    create endpoints /auth and /refresh

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from core.services.auth_service import AuthService
@@ -25,13 +26,13 @@ def create_app():
         if db.is_closed():
             db.connect()
 
-    # PING
+    # /PING
     @app.route('/ping', methods=['GET'])
     def ping():
         # Endpoint para probar la conexion al servidor
         return jsonify({"message": "Servidor conectado correctamente"}), 200
 
-    # LOGIN
+    # /LOGIN
     @app.route('/login', methods=['POST'])
     def login():
         data = request.get_json()
@@ -41,9 +42,10 @@ def create_app():
         mail = data.get('mail')
         password = data.get('password')
         sign = data.get('sign')
+
         # Validacion de datos necesarios
         if not mail or not password or not sign:
-            return jsonify({"error": "Faltan credenciales."}), 400
+            return jsonify({"error": "Faltan credenciales o la firma."}), 400
 
         try:
             # Validacion de la firma
@@ -54,16 +56,17 @@ def create_app():
             # Verificacion de contrasenya
             if not check_password_hash(user.password, password):
                 return jsonify({"error": "Usuario o contraseña incorrectos."}), 401
-
+            # Generar tokens
             tokens = AuthService.generate_tokens(user.id)
+
             return jsonify({
                 "user": {
                 "username": user.username,
                 "person_id": user.id,
                     "activated": True
              },
-                "token": tokens["access_token"],
-                "refreshToken": tokens["refresh_token"]
+                "access_token": tokens["access_token"],
+                "refresh_token": tokens["refresh_token"]
             }), 200
 
         except ValueError as e:
@@ -72,7 +75,7 @@ def create_app():
         except UserModel.DoesNotExist:
             return jsonify({"error": "Usuario o contraseña incorrectos."}), 401
 
-    # REGISTER
+    # /REGISTER
     @app.route("/register", methods=['POST'])
     def register():
         data = request.get_json()
@@ -91,6 +94,36 @@ def create_app():
         hashed_password = generate_password_hash(password)
         user = UserModel.create(username=username, mail=mail, password=hashed_password)
         return jsonify({"message": f"Usuario {user.username} registrado correctamente"}), 201
+
+    # /AUTH
+    @app.route("/auth", methods=['POST'])
+    @jwt_required()
+    def auth():
+
+        try:
+            user_id = get_jwt_identity()
+            return ({"message": f"Token valido, user_id = {user_id}"}), 200
+        except Exception as e:
+            return jsonify({"error": "Token invalido"}), 400
+
+    # / REFRESH
+    @app.route('/refresh', methods=['POST'])
+    @jwt_required(refresh=True)
+    def refresh():
+        data = request.get_json()
+        if not data or "refreshToken" not in data:
+            return jsonify({"error": "Falta el token de refresco"}), 401
+        refresh_token = data["refreshToken"]
+
+        try:
+            token = AuthService.verify_refresh_token(refresh_token)
+            new_tokens = AuthService.generate_tokens(token.user_id.id)
+            token.update_date = datetime.utcnow()
+            token.save()
+
+            return jsonify(new_tokens), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 401
 
     return app
 
