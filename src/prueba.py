fcc0ddb3d1cf75018dfbeb3d65f4f6d09fa1f0ed
Silx79 <javier.mira46@gmail.com>
2025-01-21 18:08:31 +0100

    commit

--- src/prueba.py
+++ src/prueba.py
@@ -1,7 +1,4 @@
 from core.services.auth_service import AuthService
-from infrastructure.db.database import db
-from infrastructure.db.models.auth_model import AuthModel
-
 
 mail ="javier@mail.com"
 password ="1234"
