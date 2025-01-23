79bfe0063d51d59ebe9f64ad701868f5b988162f
Silx79 <javier.mira46@gmail.com>
2025-01-23 07:27:20 +0100

    fix code. quit self in     @staticmethod

--- src/core/exceptions/mail_format_exception.py
+++ src/core/exceptions/mail_format_exception.py
@@ -3,14 +3,11 @@
 
 class MailFormatException(Exception):
     # Pasamos el mail por parametro
-    def __init__(self, mail: str):
-        self.mail = mail
-        super().__init__(f"Formato de correo electronico no valido. {mail}")
+    def __init__(self):
+        super().__init__(f"Formato de correo electronico no valido.")
 
     @staticmethod
-    def validate_mail_format(self, mail: str):
+    def validate_mail_format(mail: str):
         # El mail debe tener @ y ".". En caso contrario lanzará una excepcion
         if not "@" in mail or not "." in mail:
-            raise MailFormatException(mail)
-        else:
-            pass
+            raise MailFormatException()
