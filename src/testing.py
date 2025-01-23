3acb8e25f6237bd34e90b97aa82e86794235bcff
Silx79 <javier.mira46@gmail.com>
2025-01-23 06:39:30 +0100

    rename file

--- src/testing.py
+++ src/testing.py
@@ -1,11 +1,12 @@
+from core.exceptions.mail_format_exception import MailFormatException
 from core.services.auth_service import AuthService
 
-mail ="javier@mail.com"
+mail ="javiermailcom"
 password ="1234"
 
 mail = mail.strip()
 password = password.strip()
-
+validate_mail = MailFormatException(mail)
 valid_sing = AuthService.generate_sign(mail, password)
 print(f"Firma esperada: {valid_sing}")
 
